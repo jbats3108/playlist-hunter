55a3c43672ad22c9606528c21269b4685f9e71a3
Jamie Batabyal <jamie.batabyal@tillo.io>
2022-08-16 09:53:00 +0100

    it_displays_all_playlists

--- tests/Feature/Controllers/PlaylistControllerTest.php
+++ tests/Feature/Controllers/PlaylistControllerTest.php
@@ -32,4 +32,31 @@ class PlaylistControllerTest extends TestCase
             $response->assertSee($song->artist);
         });
     }
+
+    /** @test */
+    public function it_displays_all_playlists()
+    {
+        // Given
+        $playlists = Playlist::factory()->count(3)->create([]);
+
+        $playlists->each(function ($playlist) {
+            Song::factory()->count(3)->create(
+                [
+                    'playlist_id' => $playlist->id
+                ]
+            );
+        });
+
+        // When
+        $response = $this->get('playlists')->assertSuccessful();
+
+        // Then
+        $playlists->each(function ($playlist) use ($response) {
+            $response->assertSee($playlist->title);
+            $playlist->songs()->each(function ($song) use ($response) {
+                $response->assertSee($song->title);
+                $response->assertSee($song->artist);
+            });
+        });
+    }
 }

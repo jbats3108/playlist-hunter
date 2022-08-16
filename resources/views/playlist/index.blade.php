d251c0763cfeb9065c913103aa358704125082fe
Jamie Batabyal <jamie.batabyal@tillo.io>
2022-08-16 09:57:19 +0100

    Display all playlists and their songs

---
+++ resources/views/playlist/index.blade.php
@@ -0,0 +1,6 @@
+@foreach($playlists as $playlist)
+    {{$playlist->title}}
+    @foreach($playlist->songs()->get() as $song)
+        {{$song->title}} by {{$song->artist}}
+    @endforeach
+@endforeach

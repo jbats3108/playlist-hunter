7f8e794ee107b3e157fcc35948c4d624c13dc246
Jamie Batabyal <jamie.batabyal@tillo.io>
2022-08-18 12:02:22 +0100

    it_returns_a_logged_in_user_to_the_dashboard

--- tests/Feature/Auth/SpotifyControllerTest.php
+++ tests/Feature/Auth/SpotifyControllerTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Feature\Auth;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\User as SpotifyUser;
+use Mockery;
 use Tests\TestCase;
 
 class SpotifyControllerTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function it_redirects_to_the_spotify_login_page()
     {
@@ -14,4 +19,28 @@ class SpotifyControllerTest extends TestCase
 
         $this->call('GET', '/spotify/login')->isRedirection();
     }
+
+    /** @test */
+    public function it_returns_a_logged_in_user_to_the_dashboard()
+    {
+        $spotifyUser = Mockery::mock(SpotifyUser::class);
+
+        $spotifyUser->shouldReceive('getId')
+            ->andReturn('fooBar')
+            ->shouldReceive('getEmail')
+            ->andReturn('foo@bar.com');
+
+        Socialite::shouldReceive('driver->user')->andReturn($spotifyUser);
+
+        $response = $this->call('GET', '/spotify/callback');
+
+        $this->assertDatabaseHas('users',
+        [
+            'spotify_id' => 'fooBar'
+        ]);
+
+        $response->assertRedirect('dashboard');
+
+    }
+
 }

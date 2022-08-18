aa06e6338b49baf875022c889d8e71232e3c96f4
Jamie Batabyal <jamie.batabyal@tillo.io>
2022-08-18 09:47:22 +0100

    Update docblocks to cover typings and eloquent methods

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -2,13 +2,33 @@
 
 namespace App\Models;
 
+use Database\Factories\PlaylistFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
 /**
+ * App\Models\Playlist
+ *
  * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @property string $title
+ * @property-read Collection|Song[] $songs
+ * @property-read int|null $songs_count
+ * @method static PlaylistFactory factory(...$parameters)
+ * @method static Builder|Playlist newModelQuery()
+ * @method static Builder|Playlist newQuery()
+ * @method static Builder|Playlist query()
+ * @method static Builder|Playlist whereCreatedAt($value)
+ * @method static Builder|Playlist whereId($value)
+ * @method static Builder|Playlist whereTitle($value)
+ * @method static Builder|Playlist whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class Playlist extends Model
 {
--- app/Models/Song.php
+++ app/Models/Song.php
@@ -2,15 +2,33 @@
 
 namespace App\Models;
 
+use Database\Factories\SongFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
+ * App\Models\Song
+ *
  * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property int|null $playlist_id
  * @property string $title
  * @property string $artist
- * @property string $url
+ * @method static SongFactory factory(...$parameters)
+ * @method static Builder|Song newModelQuery()
+ * @method static Builder|Song newQuery()
+ * @method static Builder|Song query()
+ * @method static Builder|Song whereCreatedAt($value)
+ * @method static Builder|Song whereId($value)
+ * @method static Builder|Song wherePlaylistId($value)
+ * @method static Builder|Song whereTitle($value)
+ * @method static Builder|Song whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class Song extends Model
 {
